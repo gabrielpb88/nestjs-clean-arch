4b4cd2cdbd2c2daa5990d3000f4a2f9ad231cc60
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-05-09 12:21:01 -0300

    test: add route repository interface and implementation

---
+++ src/infra/db/route-in-memory.repository.spec.ts
@@ -0,0 +1,24 @@
+import { RouteInMemoryRepository } from './route-in-memory.repository';
+import { Route, RouteProps } from '@domain/entities/route.entity';
+
+describe('RouteInMemoryRepository', () => {
+  let repository: RouteInMemoryRepository;
+
+  beforeAll(() => {
+    repository = new RouteInMemoryRepository();
+  });
+
+  it('should insert a new route', async () => {
+    const routeProps: RouteProps = {
+      title: 'any title',
+      startPosition: { lat: 1, lng: 1 },
+      endPosition: { lat: 3, lng: 3 },
+    };
+    const route = new Route(routeProps);
+
+    expect(repository.items).toHaveLength(0);
+    await repository.insert(route);
+    expect(repository.items).toHaveLength(1);
+    expect(repository.items).toEqual([route]);
+  });
+});
